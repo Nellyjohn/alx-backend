d307d40fb7f82b792442f72e3e8cb26ba0704c85
veeviyan <okpala_vivian@yahoo.com>
2024-07-22 15:03:02 +0100

    Write a function named index_range that takes two integer arguments page and page_size

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,5 +6,5 @@ def index_range(page: int, page_size: int) -> tuple[int, int]:
     """Return a tuple of start and end indexes for a given page and page_size.
     Pages and indexes are 1-indexed."""
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size
+    end_index = page * page_size
     return start_index, end_index

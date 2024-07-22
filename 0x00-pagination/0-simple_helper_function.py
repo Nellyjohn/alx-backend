3e3388590b5e636262fac82b31d00b88f53cf500
veeviyan <okpala_vivian@yahoo.com>
2024-07-22 15:07:22 +0100

    modifications

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Module Docs"""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Return a tuple of start and end indexes for a given page and page_size.
     Pages and indexes are 1-indexed."""
     start_index = (page - 1) * page_size
-    end_index = page * page_size
+    end_index = start_index + page_size
     return start_index, end_index
